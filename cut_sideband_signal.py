f3cff5cb92923f66d727de08240c463907fb1519
Horst Egon <benedikt.maier@cern.ch>
2021-09-13 11:35:40 -0400

    cms

--- cut_sideband_signal.py
+++ cut_sideband_signal.py
@@ -2,51 +2,62 @@ import h5py
 import numpy as np
 from re import sub
 import sys
-import pathlib
-
-deta_jj = 1.4
 
 filename = sys.argv[1]
-outdir = sys.argv[2]
+deta_jj = 1.4
+
+def xyze_to_eppt(constituents):
+    ''' converts an array [N x 100, 4] of particles
+from px, py, pz, E to eta, phi, pt (mass omitted)
+    '''
+    PX, PY, PZ, E = range(4)
+    pt = np.sqrt(np.float_power(constituents[:,:,PX], 2) + np.float_power(constituents[:,:,PY], 2), dtype='float32') # numpy.float16 dtype -> float power to avoid overflow
+    eta = np.arcsinh(np.divide(constituents[:,:,PZ], pt, out=np.zeros_like(pt), where=pt!=0.), dtype='float32')
+    phi = np.arctan2(constituents[:,:,PY], constituents[:,:,PX], dtype='float32')
+
+    return np.stack([pt, eta, phi], axis=2)
 
 with h5py.File(filename, "r") as f:
 
-    path = pathlib.PurePath(filename)
-    lastFolder = path.parent.name
-
-    fshort = filename.split("/")[-1]
-    f_sig = sub('\.h5$', '_sig.h5', fshort)
-    f_side = sub('\.h5$', '_side.h5', fshort)
-    
-    dir_sig = outdir+'/'+lastFolder+'_sig/'
-    dir_side = outdir+'/'+lastFolder+'_side/'
-
-    for d in [dir_sig,dir_side]:
-        if not os.path.exists(d):
-            os.makedirs(d)
-
-    sig_hf = h5py.File(dir_sig+f_sig, 'w')
-    side_hf = h5py.File(dir_side+f_side, 'w')
+    f_sig = sub('\.h5$', '_sig.h5', filename)
+    f_side = sub('\.h5$', '_side.h5', filename)
+    sig_hf = h5py.File(f_sig, 'w')
+    side_hf = h5py.File(f_side, 'w')
 
     # List all groups
     #print("Keys: %s" % f.keys())
+    #a_group_key = list(f.keys())[0]
     #print(f["jet_kinematics"][:,1:2][:,0])
-
-    # Input categories explained here
-    # https://github.com/case-team/CASEUtils/tree/master/H5_maker
 
     sig_mask = (f["jet_kinematics"][:,1:2][:,0] < deta_jj)
     side_mask = (f["jet_kinematics"][:,1:2][:,0] > deta_jj)
 
+    #print(f["jet_kinematics"][:,1:2][:,0])
+    #print(np.reshape(f["jet_kinematics"][:,2],(-1,1)))
     # Preparing signal region files
-    sig_pf1 = np.array(f["jet1_PFCands"])[sig_mask].astype(np.float16)
-    sig_pf2 = np.array(f["jet2_PFCands"])[sig_mask].astype(np.float16)
-    sig_jj = np.array(f["jet_kinematics"])[sig_mask].astype(np.float16)
-    sig_truth = np.array(f["truth_label"])[sig_mask].astype(np.float16)
-    side_pf1 = np.array(f["jet1_PFCands"])[side_mask].astype(np.float16)
-    side_pf2 = np.array(f["jet2_PFCands"])[side_mask].astype(np.float16)
-    side_jj = np.array(f["jet_kinematics"])[side_mask].astype(np.float16)
-    side_truth = np.array(f["truth_label"])[side_mask].astype(np.float16)
+
+    # First jet
+    pf1 = xyze_to_eppt(np.array(f["jet1_PFCands"]))
+    pf1[:,:,0] = pf1[:,:,0]/np.reshape(np.array(f["jet_kinematics"][:,2]),(-1,1))
+    pf1[:,:,1] = pf1[:,:,1]-np.reshape(np.array(f["jet_kinematics"][:,3]),(-1,1))
+    pf1[:,:,2] = pf1[:,:,2]-np.reshape(np.array(f["jet_kinematics"][:,4]),(-1,1))
+    pf1 = pf1.astype(np.float32)
+    sig_pf1 = pf1[sig_mask].astype(np.float32)
+    side_pf1 = pf1[side_mask].astype(np.float32)
+
+    # Second jet
+    pf2 = xyze_to_eppt(np.array(f["jet2_PFCands"]))
+    pf2[:,:,0] = pf2[:,:,0]/np.reshape(np.array(f["jet_kinematics"][:,6]),(-1,1))
+    pf2[:,:,1] = pf2[:,:,1]-np.reshape(np.array(f["jet_kinematics"][:,7]),(-1,1))
+    pf2[:,:,2] = pf2[:,:,2]-np.reshape(np.array(f["jet_kinematics"][:,8]),(-1,1))
+    pf2 = pf2.astype(np.float32)
+    sig_pf2 = pf2[sig_mask].astype(np.float32)
+    side_pf2 = pf2[side_mask].astype(np.float32)
+
+    sig_jj = np.array(f["jet_kinematics"])[sig_mask].astype(np.float32)
+    sig_truth = np.array(f["truth_label"])[sig_mask].astype(np.float32)
+    side_jj = np.array(f["jet_kinematics"])[side_mask].astype(np.float32)
+    side_truth = np.array(f["truth_label"])[side_mask].astype(np.float32)
 
     sig_hf.create_dataset('jet1_PFCands', data=sig_pf1)
     sig_hf.create_dataset('jet1_PFCands_shape', data=sig_pf1.shape)
@@ -69,3 +80,19 @@ with h5py.File(filename, "r") as f:
     sig_hf.close()
     side_hf.close()
 
+    # Get the data
+    #data = list(f[a_group_key])
+    #for i in data:
+    #   print(i)
+
+    #sig_idx = []
+    #side_idx = []
+    #data = list(f['jet_kinematics'])
+    #counter = 0
+    #for i in data:
+    #    print(i[1:2][0])
+    #    if i[1:2][0] > 1.4:
+    #        side_idx.append(counter)
+    #    else:
+    #        sig_idx.append(counter)
+    #    counter +=1 
